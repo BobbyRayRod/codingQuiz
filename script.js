d77541c99cfbd9ad142c486b6af199e58b7a5b59
Bobby <bobbyrayrod92@gmail.com>
2022-11-03 08:55:56 -0600

    Created indexs for html, js, and css

---
+++ script.js
@@ -0,0 +1,32 @@
+// select start button
+
+
+//add event listner
+
+
+// when clicked show first question
+
+
+// show possible answers
+
+
+// find out what the user chose
+
+
+// compare user choice to correct answer
+
+
+// if this is correct add to score
+
+//create timer
+
+
+// else lower remaining time
+
+
+// bring up next question
+
+// when out of questions hide quiz screen
+// show end screen
+//get innitials from user
+// save high score to local storage
