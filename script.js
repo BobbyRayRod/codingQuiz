9cf3b13cf091eff1e9a741b519303b198d664851
Bobby <bobbyrayrod92@gmail.com>
2022-12-26 20:43:07 -0700

    built out, and connected multiple question, along with randomized question order. having issues with the forEach in showQuestion line 33

--- script.js
+++ script.js
@@ -1,12 +1,122 @@
 // select start button
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions, currentQuestionIndex
 
 //add event listener
-
-
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 // when clicked show first question
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(()=> Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
 
+function setNextQuestion(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children). forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1){
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'What is 1+1?',
+        answers: [
+            {text: '2', correct: true},
+            {text: '14', correct: false},
+            {text: '7', correct: false},
+            {text: '1', correct: false}
+        ]
+    },
+    {
+        question: 'Is web development fun?',
+        answer: [
+            {text: 'No', correct: false},
+            {text: 'Yes', correct: true},
+            {text: 'Kinda', correct: false},
+            {text: 'Meh', correct: false},
+        ]
+    },
+    {
+        question: 'What do developers do for fun?',
+        answer: [
+            {text: 'Fish', correct: true},
+            {text: 'Game', correct: true},
+            {text: 'Build Robots', correct: true},
+            {text: 'Sleep', correct: true},
+        ]
+    },
+    {
+        question: 'Which is the best resource for web developers listed below?',
+        answer: [
+            {text: 'Library', correct: false},
+            {text: 'Google', correct: true},
+            {text: 'Farmers Market', correct: false},
+            {text: 'Grandma', correct: false},
+        ]
+    },
+]
 // show possible answers
 
 
@@ -27,6 +137,11 @@
 // bring up next question
 
 // when out of questions hide quiz screen
+
 // show end screen
-//get innitials from user
+
+//get initials from user
+
 // save high score to local storage
+
+// Questions to be used in quiz
